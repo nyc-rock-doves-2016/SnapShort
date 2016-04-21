922c9f9bf3bb7cb4296149ad5cc4722e03826349
Kevin Perkins <email": null>
2016-04-20 21:06:45 -0400

    create comments and votes routes + erbs

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,14 @@
+get '/prompts/:id/comments' do
+
+end
+
+post '/prompts/:id/comments' do
+
+end
+
+
+
+
+# Votes:
+# post 'prompts/:id/comments -create comment
+# post 'shorts/:id/shorts -create comment
