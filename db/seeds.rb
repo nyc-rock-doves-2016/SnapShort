18a0616a96444bd345209e8c0236873c4743a29d
Emily Osowski <emosowski@gmail.com>
2016-04-21 14:51:06 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,21 @@
-User.create!(username: "Test", email: "test@example.com", password: "password")
+User.create!(username: "Emily", email: "emily@emily.com", password: "abc123")
 
-Prompt.create!(title: "prompt title", creator_id: 1)
+User.create!(username: "David", email: "david@david.com", password: "abc123")
 
-Short.create!(body: "This is a body", creator_id: 1, prompt_id: 1)
+User.create!(username: "Kevin", email: "kevin@kevin.com", password: "abc123")
+
+Prompt.create!(title: "A babysitter is snooping around her employer's house and finds a disturbing photograph...", creator_id: 1)
+
+Prompt.create!(title: "Bob Barker falls in love.", optional: "Have fun!" creator_id: 2)
+
+Prompt.create!(title: "Identical twins, a party invitation, and a locked closet.", creator_id: 3)
+
+Short.create!(body: "Bob Barker looked at the spotty teapot in his hands and felt anxious.He walked over to the window and reflected on his sunny surroundings. He had always loved snooty Dallas with its wild, wonky waters. It was a place that encouraged his tendency to feel anxious.Then he saw something in the distance, or rather someone. It was the figure of Cuthbert Bogtrotter. Cuthbert was a cowardly deity with charming fingernails and beautiful fingers. Bob gulped. He glanced at his own reflection. He was a smart, charming, tea drinker with solid fingernails and handsome fingers. His friends saw him as a gleaming, gentle god. Once, he had even helped a breezy deaf person cross the road.But not even a smart person who had once helped a breezy deaf person cross the road, was prepared for what Cuthbert had in store today.The wind blew like sitting tortoises, making Bob ambivalent. As Bob stepped outside and Cuthbert came closer, he could see the pickled glint in his eye.I am here because I want affection, Cuthbert bellowed, in a bold tone. He slammed his fist against Bob's chest, with the force of 5397 pigeons. I frigging love you, Bob Barker.Bob looked back, even more ambivalent and still fingering the spotty teapot.Cuthbert, let's move in together, he replied. They looked at each other with confident feelings, like two regurgitated, resonant rabbits singing at a very admirable Valentine's meal, which had jazz music playing in the background and two mean uncles to the beat. Bob studied Cuthbert's charming fingernails and beautiful fingers. Eventually, he took a deep breath. I'm sorry, but I can't give you affection, he explained, in pitying tones. Cuthbert looked active, his body raw like a gifted, gloopy guillotine. Bob could actually hear Cuthbert's body shatter into 9834 pieces. Then the cowardly deity hurried away into the distance.Not even a cup of tea would calm Bob's nerves tonight.", creator_id: 3, prompt_id: 2)
+
 
 Vote.create!(votable_type: "Short", votable_id: 1, value: 1)
 
 Vote.create!(votable_type: "Prompt", votable_id: 1, value: 1)
 
-Comment.create!(body: "this is a comment", commentable_id: 1, commentable_type: "Short")
+Comment.create!(body: "I can't determine Bob's motivation, I think you could develop that further.", commentable_id: 1, commentable_type: "Short")
 
-Comment.create!(body: "this is a prompt comment", commentable_id: 1, commentable_type: "Prompt")
